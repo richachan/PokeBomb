dd9cd56146815f89ce2caec2a92e3c532e5d5d24
richachan <rc723731@gmail.com>
2025-01-07 22:35:09 -0500

    dd

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { JSX, useEffect, useState } from 'react';
 import { io, Socket } from 'socket.io-client';
 
 // Initialize the socket connection
